b290294fb3c086671cadb81462a57a422342c5ee
Quentin <Quentin@192.168.1.26>
2016-05-29 19:49:55 +0200

    Phase 4

--- src/jpu2016/dogfight/controller/DogfightController.java
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -1,10 +1,17 @@
 package jpu2016.dogfight.controller;
 
+import jpu2016.dogfight.view.DogfightView;
+
 public class DogfightController {
 
 	public static void main(String[] args) {
 		
-		
+		final DogfightModel dogfightModel = new DogfightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel,
+		dogfightModel);
+		dogfightController.setViewSystem(dogfightView);
+		dogfightController.play();
 
 	}
 
