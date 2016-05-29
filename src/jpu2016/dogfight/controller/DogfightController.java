3a73443ed0d514bf53d62ae9d304d294bea477bf
Quentin <Quentin@192.168.1.26>
2016-05-29 20:24:27 +0200

    Phase 4

--- src/jpu2016/dogfight/controller/DogfightController.java
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -1,15 +1,48 @@
 package jpu2016.dogfight.controller;
 
 import jpu2016.dogfight.view.DogfightView;
+import jpu2016.dogfight.view.IViewSystem;
 import jpu2016.dogfight.model.DogfightModel;
+import jpu2016.dogfight.model.IDogfightModel;
 
 public class DogfightController {
 
 	public static void main(String[] args) {
 		
+		int TIME_SLEEP = 30;
+
+		public DogfightController(IDogfightModel dogfightModel) {
+
+		}
+
+		public void orderPerform(UserOrder userOrder) {
+			return;
+
+		}
+
+		public void play() {
+			return null;
+
+		}
+
+		public void setViewSystem(IViewSystem viewSystem) {
+			return;
+
+		}
+
+		private void launchMissile(int player) {
+			return;
+
+		}
+
+		private void gameLoop() {
+			return;
+
+		}
+		
 		final DogfightModel dogfightModel = new DogfightModel();
 		final DogfightController dogfightController = new DogfightController(dogfightModel);
-		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel);
 		dogfightController.setViewSystem(dogfightView);
 		dogfightController.play();
 
